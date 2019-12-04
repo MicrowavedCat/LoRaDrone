7d8f13e9d95ac9c1878a3d3f5e27be7f96208da6
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2019-12-04 08:55:47 +0000

    Update communication.c

--- Code_drone/src/communication.c
+++ Code_drone/src/communication.c
@@ -2,7 +2,7 @@
 /* Variables globales définissant les états de la connexion drone-télécommande */
 #define PAIR "pair\4"
 #define LINK "link\4"
-#define CONNECTED "connected\4"
+#define CONNECTED "connect\4"
 #define LOST "lost\4"
 #define STOP "stop\4" 
 #define END "end\4" 
