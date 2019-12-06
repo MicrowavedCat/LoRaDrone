816c1fc292eb09fc44184b242879d1167b059a64
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2019-12-06 08:49:37 +0000

    Add files via upload

---
+++ Code_drone/header/controle.h
@@ -0,0 +1,16 @@
+#ifndef CONTROLE_H
+#define CONTROLE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+
+/* Flux de données permettant, sans fil et à distance, la communication série */
+#define BUS "i2c-1"
+
+void i2c(void);
+
+#endif
