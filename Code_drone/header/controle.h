a418723ef1fb1d347320e0ed5d36b8440a098c62
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2019-12-06 12:07:20 +0000

    Update controle.h

--- Code_drone/header/controle.h
+++ Code_drone/header/controle.h
@@ -3,12 +3,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <linux/i2c-dev.h>
 #include <sys/ioctl.h>
-#include <fcntl.h>
 
-/* Flux de données permettant, sans fil et à distance, la communication série */
+/* Flux de données permettant la détection des dipositifs branchés au Raspberry. */
 #define BUS "/dev/i2c-1"
 
 void i2c(void);
