65b798cfa40f800937049fb3bb6b8427b1cd6820
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2019-12-06 16:18:21 +0000

    Update communication.h

--- Code_drone/header/communication.h
+++ Code_drone/header/communication.h
@@ -1,16 +1,7 @@
 #ifndef COMMUNICATION_H
 #define COMMUNICATION_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <errno.h>
-#include <fcntl.h>
 #include <pthread.h>
-/* Librairie de communication série */
-#include <wiringPi.h>
-#include <wiringSerial.h>
 
 /* Flux de données permettant, sans fil et à distance, la communication série */
 #define FLUX "/dev/ttyAMA0"
