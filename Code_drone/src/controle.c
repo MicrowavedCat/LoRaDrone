ae48fbc12a41032fe69ca4d5ad1562f2b7b33893
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2019-12-06 19:42:20 +0100

    Update controle.c

--- Code_drone/src/controle.c
+++ Code_drone/src/controle.c
@@ -12,23 +12,23 @@ void i2c(void) {
   L'utilisation d'une adresse qui n’est pas déjà utilisée sur les devices est nécessaires. */
   ioctl(fd, I2C_SLAVE, 0x53);
   char config[2];
-  /* 0x2C (soit ',' et 44), sélectionne du registre de taux de bande passante */
+  /* (0x2C = 44) sélectionne du registre de taux de bande passante */
   config[0] = 0x2C;
-  /* 0x0A (soit '\n' et 10) correpond au débit de données de sortie (100 Hz) */
+  /* (0x0A = 10) correpond au débit de données de sortie (100 Hz) */
   config[1] = 0x0A;
   /* Ecrire la configuration dans le flux de taille 2 */
   write(fd, config, 2); /* Et 44 + 10 < 83 */
 
-  /* 0x2D (soit '-' et 45), configure le registre de controle de puissance */
+  /* (0x2D = 45), configure le registre de controle de puissance */
   config[0] = 0x2D;
-  /* 0x08 (soit '\b' et 8), mise en veille automatique */
+  /* (0x08 = 8), mise en veille automatique */
   config[1] = 0x08;
-  /* Ecrire la configuration dans le flux de taille 2 */
+  /* Ecrirture de la configuration dans le flux de taille 2 */
   write(fd, config, 2); /* Et 45 + 8 < 83 */
 
-  /* 0x31 (soit '1' et 49), sélectionne le format de données */
+  /* (0x31 = 49), sélectionne le format de données */
   config[0] = 0x31;
-  /* 0x08 (soit '\b' et 8), autotest désactivé, plage + ou - équivalente à 2g */
+  /* (0x08 = 8), autotest désactivé, plage + ou - équivalente à 2g */
   config[1] = 0x08;
   /* Ecrire la configuration dans le flux de taille 2 */
   write(fd, config, 2); /* Et 49 + 8 < 83 */
