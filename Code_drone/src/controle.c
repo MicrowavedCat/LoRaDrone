c65e1fd928a433d46dc1050dc5eac37d58af0d45
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2019-12-08 14:54:26 +0100

    Update controle.c

--- Code_drone/src/controle.c
+++ Code_drone/src/controle.c
@@ -6,6 +6,7 @@
 et de relever, les coordonnées de l'accéléromètre ADXL345 */
 void i2c(void) {
   int fd;
+  /* Ouverture du bus i2c en lecutre et écriture */
   if ((fd = open(BUS, O_RDWR)) < 0){
     perror("Erreur communication ");
     exit(1);
