e205eda2c7b149043f97c6994408e42fe8be4e4e
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2019-12-11 14:48:21 +0400

    Update controle.c

--- Code_drone/src/controle.c
+++ Code_drone/src/controle.c
@@ -19,8 +19,7 @@ void i2c(void) {
   -> L'argument [1] est un descripteur de flux de donnée, en l'occurence pour le fichier "/dev/i2c-1",
   fichier permettant d'accéder à tous les appareils d'un adaptateur depuis l'espace utilisateur,
   car habituellement les périphériques i2c sont contrôlés par un driver du noyau (Kernel).
-  -> L'argument [2] est une requête relative à un périphérique, ici il s'agit du système maitre-esclave permettant,
-  sur 7 bits, de changer l'adresse d'un périphérique.   
+  -> L'argument [2] est une requête relative à un périphérique.   
   -> L'argument [3] est est l'adresse par défault de connection de l'ADXL345,
   en vérifiant dans le chemin d'accés "/sys/class/i2c-adapter/", on obtient 53. */
   if(ioctl(fd, I2C_SLAVE, 0x53) < 0){
