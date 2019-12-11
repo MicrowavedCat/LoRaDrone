95a94ee65c304c9c5f58418f1508ab7190559118
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2019-12-11 08:35:32 +0000

    Update controle.c

--- Code_drone/src/controle.c
+++ Code_drone/src/controle.c
@@ -21,8 +21,8 @@ void i2c(void) {
   car habituellement les périphériques i2c sont contrôlés par un driver du noyau (Kernel).
   -> L'argument [2] est une requête relative à un périphérique, ici il s'agit du système maitre-esclave permettant,
   sur 7 bits, de définir si le maitre lit ou ecrit sur l'esclave.   
-  -> L'argument [3] est l'adresse 53 est l'adresse par défault de connection de l'ADXL345,
-  (vérifiable en faisant "sudo i2cdetect -y l" sur Raspberry). */
+  -> L'argument [3] est est l'adresse par défault de connection de l'ADXL345,
+  en vérifiant dans le chemin d'accés "/sys/class/i2c-adapter/", on obtient 53. */
   ioctl(fd, I2C_SLAVE, 0x53);
   unsigned char config[2];
   /* (0x2C = 44) sélectionne du registre de taux de bande passante */
