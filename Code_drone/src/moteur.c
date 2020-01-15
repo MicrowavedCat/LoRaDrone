52f19539c5a7697d5b6c3105c139593f4626f639
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2020-01-15 16:45:58 +0100

    Update moteur.c

--- Code_drone/src/moteur.c
+++ Code_drone/src/moteur.c
@@ -1,1 +1,15 @@
-#include "../header/config.h"
+#include "../header/moteur.h"
+
+/* Pin sur lequel on branche le moteur */
+#define PIN 4
+
+void main(int argc, char const argv*[]){
+  int position = 0, angle = 0;
+  pinMode(PIN, PWM_OUTPUT);
+  pwmSetMode(PWM_MOD_MS);
+  pwmSetClock(192);
+  angle = strtod(argv[1], NULL);
+  position = angle + 60;
+  pwmWrite(PIN, position);
+  exit(0);
+}
