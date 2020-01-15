c1b21473ab19b1d766eae3324e2343845123d1f0
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2020-01-15 16:36:55 +0100

    Create moteur.h

---
+++ Code_drone/header/moteur.h
@@ -0,0 +1,9 @@
+#ifndef COMMUNICATION_H
+#define COMMUNICATION_H
+
+#include <wiringPi.h>
+
+/* variable globale contenant le message envoyer par la télécommande */
+unsigned char *msg_recu;
+
+#endif
