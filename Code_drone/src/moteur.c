41d52a12bd5d313fd2df39957050558d3fe7e0cb
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2020-01-15 17:28:59 +0100

    Update moteur.c

--- Code_drone/src/moteur.c
+++ Code_drone/src/moteur.c
@@ -4,13 +4,13 @@
 #define PIN 4
 
 extern void main(void){
-  int position = 0, angle = 0;
+  unsigned short int position = 0, angle = 0;
   pinMode(PIN, PWM_OUTPUT);
   pwmSetMode(PWM_MOD_MS);
   pwmSetClock(192); /* Soit 10.24 ms */
   
   printf("Entrer une valeur entre 10 et 360 : ");
-  scanf("%d", angle);
+  scanf("%hu", angle);
   
   position = angle + 60;
   pwmWrite(PIN, position);
