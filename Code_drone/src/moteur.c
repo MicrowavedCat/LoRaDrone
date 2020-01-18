382c5382dc01f9b7279b4d5828f23308f57dd08e
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2020-01-18 19:47:05 +0100

    Update moteur.c

--- Code_drone/src/moteur.c
+++ Code_drone/src/moteur.c
@@ -1,24 +1,71 @@
 #include "../header/moteur.h"
 
 /* GPIO du raspberry sur lequel on branche le moteur */
-#define PIN 1
+#define PIN1 1
+#define PIN2 24
+#define PIN3 26
+#define PIN4 23
 
-extern void main(void){
-  unsigned short int position = 0, angle = 0;
-  /* Définie sur quel pin on effectue le calcule */
-  pinMode(PIN, PWM_OUTPUT);
-  
-  printf("Entrer une valeur : ");
-  scanf("%hu", &angle);
-  
-  position = angle + 60;
-  
-  while(1) {
-    /* Ecrire la puissance en impulsion que l'on veut fournir */
-    pwmWrite(PIN, 450); /* Allumé pendant 1 sec */
-    sleep(1);
-    pwmWrite(PIN, 0); /* Eteint pendant 2 sec */
-    sleep(2);
+void configuration(void) {
+  if (wiringPiSetup() == -1) {
+    printf("Erreur de déploiement de librairie\n");
+    exit(1) ;
   }
-  exit(0);
+  
+  /* Définie sur quel pin on effectue le calcul */
+  pinMode(PIN1, PWM_OUTPUT);
+  pinMode(PIN2, PWM_OUTPUT);
+  pinMode(PIN3, PWM_OUTPUT);
+  pinMode(PIN4, PWM_OUTPUT);
+  
+  static unsigned short int puissance ;
+  for (puissance = 0 ; puissance < 1024 ; puissance++) {
+    /* Ecrire la puissance en impulsion que l'on veut fournir */
+    pwmWrite (PIN1, puissance);
+    delay(1);
+    pwmWrite (PIN2, puissance);
+    delay(1);
+    pwmWrite (PIN3, puissance);
+    delay (1);
+    pwmWrite (PIN4, puissance);
+    delay (1);
+  }
+  for (puissance = 1023 ; puissance >= 0 ; puissance--) {
+    pwmWrite (PIN1, puissance);
+    delay(1);
+    pwmWrite (PIN2, puissance);
+    delay(1);
+    pwmWrite (PIN3, puissance);
+    delay (1);
+    pwmWrite (PIN4, puissance);
+    delay (1);
+  }
+  delay(1);
+}
+
+extern void main(void) {
+  configuration();
+  static const unsigned short int vitesse = 450;
+  static const unsigned short int on = 1, off = 3;
+
+  while (1) {
+    pwmWrite (PIN1, vitesse);
+    delay(1);
+    pwmWrite (PIN2, vitesse);
+    delay(1);
+    pwmWrite (PIN3, vitesse);
+    delay(1);
+    pwmWrite (PIN4, vitesse);
+    sleep(on);
+
+    pwmWrite (PIN1, 0);
+    delay(1);
+    pwmWrite (PIN2, 0);
+    delay(1);
+    pwmWrite (PIN3, 0);
+    delay(1);
+    pwmWrite (PIN4, 0);
+    sleep(off);
+    
+  }
 }
