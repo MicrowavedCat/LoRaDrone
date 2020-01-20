4c5088fa91fd9b5bafa27bfa4413175b655a6731
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2020-01-20 18:39:22 +0100

    Update moteur.c

--- Code_drone/src/moteur.c
+++ Code_drone/src/moteur.c
@@ -1,28 +1,28 @@
 #include "../header/moteur.h"
 
 /* GPIO du raspberry sur lequel on branche l'ESC d'un moteur */
-#define PIN[] = {
+static const int PIN[] = {
     1, /* Correspond au PIN physique 12 (BCM18) */
     23, /* Correspond au PIN physique 33 (BCM13) */
     24, /* Correspond au PIN physique 35 (BCM19) */
     26, /* Correspond au PIN physique 32 (BCM12) */
 };
 
-void cycle(unsigned short int valeur){
-  for(unsigned short int i = 0; i < sizeof(PIN); i++){ 
+extern void cycle(unsigned short int valeur){
+  for(unsigned short int i = 0; i < sizeof(PIN)/4; i++){ 
     /* Ecrire la puissance en impulsion que l'on veut fournir sur un GPIO */
     pwmWrite(PIN[i], valeur);
     delay(1);
   }
 }
 
-void configuration(void) {
+extern void configuration(void) {
   if (wiringPiSetup() == -1) {
     printf("Erreur de librairie\n");
     exit(1) ;
   }
   /* Configuration des 4 ESC pour les 4 moteurs */
-  for(unsigned short int i = 0; i < sizeof(PIN); i++)
+  for(unsigned short int i = 0; i < sizeof(PIN)/4; i++)
     /* Définie sur quel PIN on effectue des opérations */
     pinMode(PIN[i], PWM_OUTPUT);
   
