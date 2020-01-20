786d1ea847f3496f745f50de02285c5c2d74ff7b
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2020-01-20 19:11:14 +0100

    Update moteur.c

--- Code_drone/src/moteur.c
+++ Code_drone/src/moteur.c
@@ -1,5 +1,8 @@
 #include "../header/moteur.h"
 
+#define MAX 511
+#define MIN 0
+
 /* GPIO du raspberry sur lequel on branche l'ESC d'un moteur */
 static const int PIN[] = {
     1, /* Correspond au PIN physique 12 (BCM18) */
@@ -9,44 +12,43 @@ static const int PIN[] = {
 };
 
 extern void cycle(unsigned short int valeur){
-  for(unsigned short int i = 0; i < sizeof(PIN)/4; i++){ 
+  for(unsigned short int i = 0; i < (sizeof(PIN))/4; i++){
     /* Ecrire la puissance en impulsion que l'on veut fournir sur un GPIO */
     pwmWrite(PIN[i], valeur);
     delay(1);
   }
 }
 
-extern void configuration(void) {
+extern void calibration(void) {
   if (wiringPiSetup() == -1) {
     printf("Erreur de librairie\n");
     exit(1) ;
   }
-  /* Configuration des 4 ESC pour les 4 moteurs */
-  for(unsigned short int i = 0; i < sizeof(PIN)/4; i++)
+  /* Configuration des 4 ESC pour les 4 moteurs sur la sortie de courant */
+  for(unsigned short int i = 0; i < (sizeof(PIN))/4; i++)
     /* Définie sur quel PIN on effectue des opérations */
     pinMode(PIN[i], PWM_OUTPUT);
-  
-  static volatile unsigned short int puissance;
-  /* Configuration de la puissance par impulsions jusqu'au maximum */
-  for(puissance = 0; puissance < 1024; puissance++)
-    cycle(puissance);
-  /* Configuration de la puissance par impulsions jusqu'au minimum */
-  for(puissance = 1023; puissance >= 0; puissance--)
-    cycle(puissance);
   delay(1);
 }
 
 extern void main(void) {
-  configuration();
-  static const unsigned short int vitesse = 450;
-  static const unsigned short int on = 1, off = 3;
-
-  while(1) {
-    /* Allumé */
-    cycle(vitesse);
+  calibration();
+  static volatile unsigned short int test;
+  static const unsigned short int on = 2, off = 5;
+  printf("Entrer valeur : "); scanf("%hu", &test);
+  
+  /* Dernière étape de calibration dans le corps principale */
+  cycle(MAX);
+  sleep(on);
+  cycle(MIN);
+  sleep(off);
+  
+  while(1){
+    /* Allumer pendant 2 secondes */
+    cycle(test);
     sleep(on);
-    /* Eteint */
-    cycle(0);
+    /* Eteindre pendant 5 secondes */
+    cycle(MIN);
     sleep(off);
   }
 }
