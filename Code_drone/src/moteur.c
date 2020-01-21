bb2275f27bb819da36d5009537e1e054d2308a3c
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2020-01-21 22:47:42 +0100

    Update moteur.c

--- Code_drone/src/moteur.c
+++ Code_drone/src/moteur.c
@@ -1,5 +1,15 @@
 #include "../header/moteur.h"
 
+#define MAX 511 /* 2^9 = 511 valeurs */
+#define MIN 0
+/* GPIO du raspberry sur lequel on branche l'ESC relié à un moteur */
+#define PIN[] = {
+  1, /* Correspond au PIN physique 12 (BCM18) */
+  23, /* Correspond au PIN physique 33 (BCM13) */
+  24, /* Correspond au PIN physique 35 (BCM19) */
+  26 /* Correspond au PIN physique 32 (BCM12) */
+}
+
 /* Définit pour chaque moteur la valeur de la puissance à transmettre */
 extern void cycle(unsigned short int valeur){
   for(unsigned short int i = 0; i < 4; i++){
