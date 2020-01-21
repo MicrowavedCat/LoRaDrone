c6ac3eba0d3ca95b9cee18ed520bb2428330f770
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2020-01-21 22:47:26 +0100

    Update moteur.h

--- Code_drone/header/moteur.h
+++ Code_drone/header/moteur.h
@@ -7,14 +7,4 @@
 #include <wiringPi.h>
 #include <pthread.h>
 
-#define MAX 511 /* 2^9 = 511 valeurs */
-#define MIN 0
-/* GPIO du raspberry sur lequel on branche l'ESC relié à un moteur */
-#define PIN[] = {
-  1, /* Correspond au PIN physique 12 (BCM18) */
-  23, /* Correspond au PIN physique 33 (BCM13) */
-  24, /* Correspond au PIN physique 35 (BCM19) */
-  26 /* Correspond au PIN physique 32 (BCM12) */
-}
-
 #endif
