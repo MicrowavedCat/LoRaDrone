61199f78edea0e41e706c046b9221f621bac3b99
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2020-01-21 22:29:06 +0100

    Update moteur.h

--- Code_drone/header/moteur.h
+++ Code_drone/header/moteur.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <wiringPi.h>
+#include <pthread.h>
 
 /* variable globale contenant le message envoyer par la télécommande */
 unsigned char *msg_recu;
