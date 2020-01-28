42311d0c61d1aefe43eb3f3421da4987044883a4
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2020-01-28 14:54:30 +0100

    Update distance.c

--- Code_drone/src/distance.c
+++ Code_drone/src/distance.c
@@ -28,7 +28,7 @@ static volatile char *addr  = NULL;
 static volatile uint32_t *gpio = NULL;
 static short int pin = 0;
 
-/* Echo */
+/* Echo de l'onde sonore, émission + récéption */
 static void ping(long temps){
   struct timespec wait;
   wait.tv_sec = temps / (1000 * 1000);
@@ -37,86 +37,86 @@ static void ping(long temps){
 }
 
 static void config_memoire(const char *argv[]){
-   if((fd = open("/dev/mem", O_RDWR|O_SYNC)) < 0) {
-      printf("Conseil : \"sudo %s\"\n", argv[0]);
-      exit(1);
-   }
-   if((addr = malloc(TAILLE + (PAGE-1))) == NULL) {
-      puts("Allocation m\u00e9 impossible");
-      exit(1);
-   }
-   if((unsigned long)addr % PAGE)
-     addr += PAGE - ((unsigned long)addr % PAGE);
-   /* Nouvelle projection dans l'espace d'adressage virtuel du processus appelant */
-   projection = (unsigned char *)mmap(
-      /* Adresse de départ pour placer projection */
-      (caddr_t)addr, TAILLE, /* Longueur de la projection */
-      /* Lire le contenu de la zone mémoire, et y écrire */
-      PROT_READ | PROT_WRITE,
-      /* Modifications de la projection visibles par les autres processus,
-      qui projettent le fichier dev/mem, en plaçant la projection exactement à l'adresse donnée */
-      MAP_SHARED | MAP_FIXED,
-      fd, OFFSET
-   );
-
-   if((long)projection < 0) {
-      puts("M\u00e9moire inacessible");
-      exit(1);
-   }
-   gpio = (volatile uint32_t *)projection;
+  if((fd = open("/dev/mem", O_RDWR|O_SYNC)) < 0) {
+    printf("Conseil : \"sudo %s\"\n", argv[0]);
+    exit(1);
+  }
+  if((addr = malloc(TAILLE + (PAGE-1))) == NULL) {
+    puts("Allocation inacessible");
+    exit(1);
+  }
+  if((unsigned long)addr % PAGE)
+    addr += PAGE - ((unsigned long)addr % PAGE);
+  /* Nouvelle projection dans l'espace d'adressage virtuel du processus appelant */
+  projection = (unsigned char *)mmap(
+    /* Adresse de départ pour placer projection */
+    (caddr_t)addr, TAILLE, /* Longueur de la projection */
+    /* Lire le contenu de la zone mémoire, et y écrire */
+    PROT_READ | PROT_WRITE,
+    /* Modifications de la projection visibles par les autres processus,
+    qui projettent le fichier dev/mem, en plaçant la projection exactement à l'adresse donnée */
+    MAP_SHARED | MAP_FIXED,
+    fd, OFFSET
+  );
+  if((long)projection < 0) {
+    puts("Memoire inacessible");
+    exit(1);
+  }
+  gpio = (volatile uint32_t *)projection;
 } 
 
+/* Sortie de la mémoire et fermeture des mappings */
 static void sortie_memoire(){
-	int ret;
-	ret = munmap(projection, TAILLE);
-	if(ret == -1){
-		puts("Erreur");
-		exit(2);
-	}
-	ret = close(fd);
-   exit(0);
+  int mapping;
+  mapping = munmap(projection, TAILLE);
+  if(mapping == -1){
+    puts("Erreur");
+    exit(2);
+  }
+  mapping = close(fd);
+  exit(0);
 }
 
 extern const unsigned short int main(unsigned short int argc, 
-char const *argv[]){
-   config_memoire(argv);
+				     char const *argv[]){
+  config_memoire(argv);
 
-   struct timespec impulsion, reception, portee;
-   double distance, echo;
+  struct timespec impulsion, reception, portee;
+  double distance, echo;
 
-   ENTREE(GPIO2);
-   SORTIE(GPIO2);
+  ENTREE(GPIO2);
+  SORTIE(GPIO2);
     
-   ENTREE(GPIO3);
-   ping(500000);
+  ENTREE(GPIO3);
+  ping(500000);
     
-   /* Envoyer toutes les 10 nano-secondes à l'émetteur */
-   CONFIG(GPIO2);
-   ping(10);
-   REMOVE(GPIO2);
+  /* Envoyer toutes les 10 nano-secondes à l'émetteur */
+  CONFIG(GPIO2);
+  ping(10);
+  REMOVE(GPIO2);
     
-   clock_gettime(CLOCK_REALTIME, &impulsion);
-   while ((GET >> GPIO3) & 0);
-   /* Boucler tant que le bit 23 est à 0, puis enregistrer le temps. */
-   clock_gettime(CLOCK_REALTIME, &impulsion);    
+  clock_gettime(CLOCK_REALTIME, &impulsion);
+  while ((GET >> GPIO3) & 0);
+  /* Boucler tant que le bit 23 est à 0, puis enregistrer le temps. */
+  clock_gettime(CLOCK_REALTIME, &impulsion);    
 
-   clock_gettime(CLOCK_REALTIME, &reception);
-   while ((GET >> GPIO3) & 1);
-   /* Boucler tant que le bit 23 est à 1, puis enregistrer le temps. */
-   clock_gettime(CLOCK_REALTIME, &reception);
+  clock_gettime(CLOCK_REALTIME, &reception);
+  while ((GET >> GPIO3) & 1);
+  /* Boucler tant que le bit 23 est à 1, puis enregistrer le temps. */
+  clock_gettime(CLOCK_REALTIME, &reception);
     
-   if((reception.tv_nsec - impulsion.tv_nsec) < 0) {
-      portee.tv_sec = reception.tv_sec - impulsion.tv_sec - 1;
-      portee.tv_nsec = 1000000000 + reception.tv_nsec - impulsion.tv_nsec;
-   }else{
-      portee.tv_sec = reception.tv_sec - impulsion.tv_sec;
-      portee.tv_nsec = reception.tv_nsec - impulsion.tv_nsec;
-   }
-   echo = ((portee.tv_sec*1000000000) + (portee.tv_nsec));
-   echo /= 1000000000;
-   /* calcul de la distance en cm */
-   distance = (echo*17150);
-   printf("Distance: %.2f cm\n", distance);
+  if((reception.tv_nsec - impulsion.tv_nsec) < 0) {
+    portee.tv_sec = reception.tv_sec - impulsion.tv_sec - 1;
+    portee.tv_nsec = 1000000000 + reception.tv_nsec - impulsion.tv_nsec;
+  }else{
+     portee.tv_sec = reception.tv_sec - impulsion.tv_sec;
+     portee.tv_nsec = reception.tv_nsec - impulsion.tv_nsec;
+  }
+  echo = ((portee.tv_sec*1000000000) + (portee.tv_nsec));
+  echo /= 1000000000;
+  /* calcul de la distance en cm */
+  distance = (echo*17150);
+  printf("Distance: %.2f cm\n", distance);
    
-   sortie_memoire();
+  sortie_memoire();
 }
