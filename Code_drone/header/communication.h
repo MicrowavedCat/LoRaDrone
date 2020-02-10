6dd57d7cd60afdc428f7e58ccf13c484c0cfec95
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2020-02-10 20:11:39 +0100

    Update communication.h

--- Code_drone/header/communication.h
+++ Code_drone/header/communication.h
@@ -11,6 +11,8 @@
 
 /* file descriptor permettant de stocker le flux de communication */
 static volatile int fd;
+/* Variable booléenne servant d'indice d'intégrité */
+static volatile unsigned short int validation = 0;
 /* variable globale contenant le message envoyer par la télécommande */
 unsigned char *msg_recu = "";
 
