7547cb16f2e1d6cbd8a54684425e81a15d2485c0
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2020-02-12 22:06:50 +0100

    Update communication.h

--- Code_drone/header/communication.h
+++ Code_drone/header/communication.h
@@ -1,6 +1,11 @@
 #ifndef COMMUNICATION_H
 #define COMMUNICATION_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <pthread.h>
 /* Librairie de communication serie sur Raspberry */
 #include <wiringPi.h>
