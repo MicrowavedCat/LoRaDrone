ce3bb61ef7df65bbba1f051ab08bbbd1414f8a0c
MicrowavedCat <38229200+MicrowavedCat@users.noreply.github.com>
2020-02-13 17:24:48 +0100

    Update moteur.h

--- Code_drone/header/moteur.h
+++ Code_drone/header/moteur.h
@@ -1,8 +1,11 @@
 #ifndef COMMUNICATION_H
 #define COMMUNICATION_H
 
-#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <pthread.h>
+#include <wiringPi.h>
 
 extern void propulsion(void);
 
