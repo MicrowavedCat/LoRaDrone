49e66f24cb129bff1e772d2480e2f6928e4ca8de
MicrowavedCat <38229200+MicrowavedCat@users.noreply.github.com>
2020-02-14 19:16:32 +0100

    Update communication.h

--- Code_drone/header/communication.h
+++ Code_drone/header/communication.h
@@ -1,21 +1,14 @@
 #ifndef COMMUNICATION_H
 #define COMMUNICATION_H
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <fcntl.h>
-#include <pthread.h>
+
 /* Librairie de communication serie sur Raspberry */
-#include <wiringPi.h>
 #include <wiringSerial.h>
 
 /* Flux de donnees permettant, avec les modules Lora, la communication serie */
 #define FLUX "/dev/ttyAMA0"
-
-/* Coordonnees de pilotage dans le message */
-static volatile unsigned short int coordonnee[6] = {0};
 
 extern void transmission(void);
 
