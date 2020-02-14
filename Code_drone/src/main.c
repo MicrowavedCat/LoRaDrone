0bce460264df5655a1e7d6c5cc262bbe05870887
MicrowavedCat <38229200+MicrowavedCat@users.noreply.github.com>
2020-02-14 19:18:15 +0100

    Update main.c

--- Code_drone/src/main.c
+++ Code_drone/src/main.c
@@ -1,7 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "../header/global.h"
 #include "../header/communication.h"
+#include "../header/moteur.h"
 
 extern void main(void){
-   transmission();
+   static pthread_t th[2];
+   pthread_create(&th[0], NULL, (void *) propulsion, NULL);
+   pthread_create(&th[1], NULL, (void *) transmission, NULL);
+   for(volatile unsigned short int i = 0; i < 2; i++) 
+     pthread_join(th[i], NULL);
 }
