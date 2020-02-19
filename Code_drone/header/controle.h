50bc9eb5a7a8ecc7aa4da4a3e412c330557f7777
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2020-02-19 19:52:01 +0100

    Update controle.h

--- Code_drone/header/controle.h
+++ Code_drone/header/controle.h
@@ -10,7 +10,7 @@
 #include <linux/i2c-dev.h>
 #include <sys/ioctl.h>
 
-/* Flux de données permettant la détection des dipositifs branchés au Raspberry. */
+/* Flux de donnees permettant la detection des dipositifs branches au Raspberry. */
 #define BUS "/dev/i2c-1"
 
 volatile unsigned short int position[3];
