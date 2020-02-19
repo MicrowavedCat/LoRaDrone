40cee8d85a443c62226af99826267548f52c3b81
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2020-02-19 19:49:16 +0100

    Create distance.h

---
+++ Code_drone/header/distance.h
@@ -0,0 +1,16 @@
+#ifndef DISTANCE_H
+#define DISTANCE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <wiringPi.h>
+
+
+extern void collision(void);
+/* Le telemetre renvoie la distance en cm,
+par rapport a un obstacle au sol. */
+volatile float distance;
+
+#endif
