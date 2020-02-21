bdc727d6c380b173e2b267beafc4e7bb61c5cc86
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2020-02-21 13:09:19 +0100

    Update main.c

--- Code_drone/src/main.c
+++ Code_drone/src/main.c
@@ -4,13 +4,17 @@
 
 /* Execution synchronise des taches */
 extern void main(void){
-    static pthread_t th[2];
+    static pthread_t th[4];
     /* Communication entre le drone et la telecommande */
     pthread_create(&th[0], NULL, (void *)transmission, NULL);
     /* Rotation des helices du drone */
     pthread_create(&th[1], NULL, (void *)propulsion, NULL);
+    /* Acceleration lineaire du drone */
+    pthread_create(&th[2], NULL, (void *)celerite, NULL);
+    /* Distance par rapport au sol du drone */
+    pthread_create(&th[3], NULL, (void *)acceleration, NULL);
     
-    for(volatile unsigned short int i = 0; i < 2; i++) 
+    for(volatile unsigned short int i = 0; i < 4; i++) 
         pthread_join(th[i], NULL);
 
     pthread_exit(NULL);
