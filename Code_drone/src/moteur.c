05d5c2cf32633f147d71abb284d2a34a2265fa0a
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2020-02-22 15:09:43 +0100

    Update moteur.c

--- Code_drone/src/moteur.c
+++ Code_drone/src/moteur.c
@@ -91,7 +91,7 @@ static void *moteur(void *puissance/*void *args*/){
 /* Permet l'atterissage automatique */
 extern void atterissage(void){
    /* Si on est au niveau du sol */
-   if((volatile unsigned short int)distance <= 4){ cycle(0); }
+   if((volatile unsigned short int)distance <= 4){ cycle(MIN); }
    else{
       /* On fait baisser progressivement dans tous les moteurs,
       la puissance du drone jusqu'a ce qu'il atterisse. */
