0900cd5538434cfa143890d29c4b8f3da48ccfed
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2020-03-05 13:47:23 +0000

    Update moteur.h

--- Code_drone/header/moteur.h
+++ Code_drone/header/moteur.h
@@ -1,6 +1,12 @@
 #ifndef MOTEUR_H
 #define MOTEUR_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+/* Librairie de communication serie sur Raspberry */
+#include <wiringPi.h>
+
 extern void cycle(volatile unsigned short int);
 extern void propulsion(void);
 extern void atterrissage(void);
